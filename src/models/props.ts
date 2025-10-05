0fde9759f66cf97835da3131758712b96420d06a
Alejandro Diez Bermejo <alejandrodiezbermejo@gmail.com>
2025-10-05 14:57:33 +0200

    Update environment variables and refactor imports for clarity

--- src/models/props.ts
+++ src/models/props.ts
@@ -1,10 +1,5 @@
 export interface BaseProps {
     id?: string | null;
     style?: string | astroHTML.JSX.CSSProperties | null;
-    class?:
-        | string
-        | Record<string, boolean>
-        | Record<any, any>
-        | Iterable<string>
-        | Iterable<any>;
+    class?: string | Record<string, boolean> | Record<any, any> | Iterable<string> | Iterable<any>;
 }
